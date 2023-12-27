d3fafaf880ae101ac83d8463e1875f9a992ed885
Samet Berkant Koca <74829377+turkmuhendisi@users.noreply.github.com>
2023-12-27 09:52:24 +0300

    Update UserController.cs

--- Fubis/Fubis/controller/UserController.cs
+++ Fubis/Fubis/controller/UserController.cs
@@ -3,6 +3,14 @@ namespace Fubis.controller
 {
     class UserController
     {
-
+       private User user;
+       UserController(User user)
+       {
+          this.user = user;
+       }
+       public string SetNationalId(string nationalId)
+       {
+          user.NationalId =  nationalId;   
+       }
     }
 }
