63ca7b75b65a409081705d4d1064560262972d24
turkmuhendisi <1turkmuhendisi@gmail.com>
2025-04-12 19:11:03 +0300

    - Remote database configuration established

---
+++ FubisApp/Model/Bicycle.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Fubis
+{
+    public class Bicycle
+    {
+        private int bicycleId;
+        private bool isAvailable;
+        private bool inMaintenance;
+        private int districtId;
+
+        public int BicycleId { get => bicycleId; set => bicycleId = value; }
+        public bool IsAvailable { get => isAvailable; set => isAvailable = value; }
+        public bool InMaintenance { get => inMaintenance; set => inMaintenance = value; }
+        public int DistrictId { get => districtId; set => districtId = value; }
+
+        public Bicycle()
+        {
+        }
+
+        public Bicycle(bool isAvailable, bool inMaintenance, int districtId)
+        {
+            this.isAvailable = isAvailable;
+            this.inMaintenance = inMaintenance;
+            this.districtId = districtId;
+        }
+    }
+}
---
+++ FubisApp/Repository/Connection.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Fubis.model
+{
+    using MySql.Data.MySqlClient;
+
+    namespace Fubis.DataAccessLayer.Repository
+    {
+        public static class Connection
+        {
+            public const string SERVER = "92.113.22.53";
+            public const string PORT = "3306";
+            public const string DATABASE_NAME = "u824624299_fubisdb";
+            public const string USERNAME = "u824624299_fubis";
+            public const string PASSWORD = "Fubis23!.";
+            private static string CONNECTION_STRING = $"Server={SERVER};Port={PORT};Database={DATABASE_NAME};Uid={USERNAME};Pwd={PASSWORD};";
+
+            public static MySqlConnection GetConnection()
+            {
+                MySqlConnection connection = new MySqlConnection(CONNECTION_STRING);
+                return connection;
+            }
+        }
+    }
+
+}
